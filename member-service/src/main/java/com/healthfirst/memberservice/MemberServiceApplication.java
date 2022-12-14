eee824f3ae9d01fbec7a7f4838a9efc6d45bc5fe
WillBurdett <williamburdett1995@gmail.com>
2022-12-14 14:43:32 +0000

    my sql app props added

--- member-service/src/main/java/com/healthfirst/memberservice/MemberServiceApplication.java
+++ member-service/src/main/java/com/healthfirst/memberservice/MemberServiceApplication.java
@@ -2,9 +2,8 @@ package com.healthfirst.memberservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
+@SpringBootApplication
 public class MemberServiceApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(MemberServiceApplication.class, args);
