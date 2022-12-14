88b002037ce2275bf46ab7e8d88d542329fb099c
SurayaaH <surayalibaan.h@gmail.com>
2022-12-14 10:44:35 +0000

    name

--- member-service/src/main/java/com/healthfirst/memberservice/MemberController.java
+++ member-service/src/main/java/com/healthfirst/memberservice/MemberController.java
@@ -1,6 +1,7 @@
 package com.healthfirst.memberservice;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -9,5 +10,10 @@ public class MemberController {
     @GetMapping("hello")
     public String helloWorld(){
         return "Hello World!";
+    }
+
+    @GetMapping("hello/{name}")
+    public String name(@PathVariable String name){
+        return "hello world " + name;
     }
 }
