5d9fa17808bd4bfd393df747db0cb8b7954bc04c
WillBurdett <williamburdett1995@gmail.com>
2022-12-14 15:36:02 +0000

    all crud methods working

--- member-service/src/main/java/com/healthfirst/memberservice/MemberService.java
+++ member-service/src/main/java/com/healthfirst/memberservice/MemberService.java
@@ -2,12 +2,10 @@ package com.healthfirst.memberservice;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
-import java.util.ArrayList;
 import java.util.List;
+
 @Service
 public class MemberService {
-
 
     private MemberRepo memberRepo;
     @Autowired
@@ -15,23 +13,12 @@ public class MemberService {
         this.memberRepo = memberRepo;
     }
 
-    private static List <Member> memberList = new ArrayList<>();
-
-   static{
-       memberList.add(new Member(1L,"bob", "marley", 44, "MALE", "bob@gmail.com",
-               "pass123", "OUTDOORS"));
-       memberList.add(new Member(2L, "ali", "marley", 30, "MALE", "ali@gmail.com",
-               "pass123", "OUTDOORS"));
-       memberList.add(new Member(3L, "sara", "will", 44, "FEMALE", "sara@gmail.com",
-               "pass123", "INDOORS"));
-       }
-
     public List<Member> getAllMembers(){
         return memberRepo.findAll();
     }
 
     public Member getMemberById(Long id) {
-       return memberList.stream().filter(member -> member.getId() == id).findFirst().get();
+       return memberRepo.findAll().stream().filter(member -> member.getId() == id).findFirst().get();
     }
 
     public void addMember(Member member) {
@@ -39,11 +26,11 @@ public class MemberService {
     }
 
     public void deleteMember(Long id) {
-       memberList.removeIf(member -> member.getId() == id);
+       memberRepo.deleteById(id);
     }
 
     public void updateMember(Long id, Member member) {
-       Member curr = memberList.stream().filter(m -> m.getId() == id).findFirst().get();
+       Member curr = memberRepo.findAll().stream().filter(m -> m.getId() == id).findFirst().get();
        curr.setFirstName(member.getFirstName());
        curr.setLastName(member.getLastName());
        curr.setAge(member.getAge());
