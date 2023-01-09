b823d560df859955fd21f77ad3065605b4d3d916
WillBurdett <williamburdett1995@gmail.com>
2023-01-09 16:03:03 +0000

    custom exception handling added and server port changed to 8081

---
+++ welcome-service/src/main/java/com/healthfirst/welcomeservice/exceptions/CustomisedResponseEntityExceptionHandler.java
@@ -0,0 +1,35 @@
+package com.healthfirst.welcomeservice.exceptions;
+
+
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import java.time.LocalDateTime;
+
+@ControllerAdvice
+public class CustomisedResponseEntityExceptionHandler extends ResponseEntityExceptionHandler {
+    //ALL EXCEPTIONS
+    @ExceptionHandler(Exception.class)
+    public final ResponseEntity<ExceptionDetails> handleAllExceptions(Exception ex, WebRequest request) throws Exception {
+        ExceptionDetails memberErrors = new ExceptionDetails(LocalDateTime.now(),
+                ex.getMessage(), request.getDescription(false));
+
+        return new ResponseEntity<ExceptionDetails>(memberErrors, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+    //VALIDATION CONSTRAINTS ERRORS
+    @Override
+    protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex, HttpHeaders headers, HttpStatus status, WebRequest request){
+        ExceptionDetails validationErrors = new ExceptionDetails(LocalDateTime.now(),
+                "Total Errors:" + ex.getErrorCount() + " First Error:" + ex.getFieldError().getDefaultMessage(), request.getDescription(false));
+
+        return new ResponseEntity(validationErrors, HttpStatus.BAD_REQUEST);
+    }
+}
+
