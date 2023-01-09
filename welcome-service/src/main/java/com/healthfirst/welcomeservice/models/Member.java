55f3e0181824f863d8c11114f7849c643b96dd91
SurayaaH <surayalibaan.h@gmail.com>
2023-01-09 16:55:40 +0000

    member model + enums

---
+++ welcome-service/src/main/java/com/healthfirst/welcomeservice/models/Member.java
@@ -0,0 +1,32 @@
+package com.healthfirst.welcomeservice.models;
+
+import com.healthfirst.welcomeservice.enums.Gender;
+import com.healthfirst.welcomeservice.enums.Interest;
+
+import lombok.*;
+
+
+import javax.validation.constraints.*;
+
+@NoArgsConstructor
+@AllArgsConstructor
+public class Member {
+
+    private Long id;
+    @Size(min = 2, max = 30)
+    private String firstName;
+    @Size(min = 2, max = 30)
+    private String lastName;
+    @Min(value = 18, message = "age must be 18 or higher")
+    @Max(value = 130, message = "age cannot be more than 130")
+    private Integer age;
+    private Gender gender;
+    @Email
+    private String email;
+    @Size(min = 8)
+
+    private String password;
+    private Interest interest;
+}
+
+
