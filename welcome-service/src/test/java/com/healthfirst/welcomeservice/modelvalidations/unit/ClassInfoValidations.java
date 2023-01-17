aba78293fdb450f9cb7eee1833aba7a04ae75171
SurayaaH <surayalibaan.h@gmail.com>
2023-01-17 14:08:20 +0000

    classinfo validations test

---
+++ welcome-service/src/test/java/com/healthfirst/welcomeservice/modelvalidations/unit/ClassInfoValidations.java
@@ -0,0 +1,83 @@
+package com.healthfirst.welcomeservice.modelvalidations.unit;
+
+import com.healthfirst.welcomeservice.enums.Interest;
+import com.healthfirst.welcomeservice.models.ClassInfo;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import javax.validation.ConstraintViolation;
+import javax.validation.Validation;
+import javax.validation.Validator;
+import javax.validation.ValidatorFactory;
+import java.time.LocalDateTime;
+import java.util.Set;
+
+import static org.junit.Assert.assertFalse;
+
+
+
+
+@RunWith(SpringRunner.class)
+@WebMvcTest(ClassInfo.class)
+public class ClassInfoValidations {
+
+    private Validator validator;
+
+    @Before
+    public void setUp() {
+        ValidatorFactory factory = Validation.buildDefaultValidatorFactory();
+        validator = factory.getValidator();
+    }
+
+     @Test
+    public void className_LeftBlankCausesError(){
+        ClassInfo classInfo = new ClassInfo(1l, "", "James",
+                Interest.DANCE,
+                LocalDateTime.of(2023,2, 10, 12, 10));
+
+         Set<ConstraintViolation<ClassInfo>> constraintViolations = validator.validate(classInfo);
+         assertFalse(constraintViolations.isEmpty());
+
+     }
+
+    @Test
+    public void  instructor_LeftBlankCausesError(){
+        ClassInfo classInfo = new ClassInfo(1l, "Boxing", "",
+                Interest.TEAMSPORTS,
+                LocalDateTime.of(2023,2, 10, 12, 10));
+
+        Set<ConstraintViolation<ClassInfo>> constraintViolations = validator.validate(classInfo);
+        assertFalse(constraintViolations.isEmpty());
+
+    }
+
+//    @Test
+//    // TODO: 17/01/2023 not sure how to test enums
+//    public void  classTypeName_LeftBlankCausesError(){
+//        ClassInfo classInfo = new ClassInfo(1l, "Boxing", "james", Interest.valueOf(),
+//                LocalDateTime.of(2023,2, 10, 12, 10));
+//
+//        Set<ConstraintViolation<ClassInfo>> constraintViolations = validator.validate(classInfo);
+//        assertFalse(constraintViolations.isEmpty());
+//
+//    }
+
+    @Test
+    public void  classTimeError_Past(){
+        ClassInfo classInfo = new ClassInfo(1l, "Boxing", "james",
+                Interest.TEAMSPORTS,
+                LocalDateTime.of(2022,2, 10, 12, 10));
+
+        Set<ConstraintViolation<ClassInfo>> constraintViolations = validator.validate(classInfo);
+        assertFalse(constraintViolations.isEmpty());
+
+    }
+
+
+
+
+
+}
