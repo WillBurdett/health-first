ace37e6160b5251c85ee66fc6b34e359f33874e1
WillBurdett <williamburdett1995@gmail.com>
2023-01-18 11:56:33 +0000

    classes-service init

---
+++ classes-service/src/main/java/com/healthfirst/classesservice/ClassesServiceApplication.java
@@ -0,0 +1,14 @@
+package com.healthfirst.classesservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+
+@SpringBootApplication (exclude = DataSourceAutoConfiguration.class)
+public class ClassesServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClassesServiceApplication.class, args);
+	}
+
+}
---
+++ classes-service/src/test/java/com/healthfirst/classesservice/ClassesServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.healthfirst.classesservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClassesServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
