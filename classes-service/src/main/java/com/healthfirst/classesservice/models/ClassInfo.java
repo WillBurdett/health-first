9a5ea0cfa4109ed0d82d7b74e13fef17c08cd15c
WillBurdett <williamburdett1995@gmail.com>
2023-01-19 12:08:14 +0000

    Mongo id generates

--- classes-service/src/main/java/com/healthfirst/classesservice/models/ClassInfo.java
+++ classes-service/src/main/java/com/healthfirst/classesservice/models/ClassInfo.java
@@ -1,5 +1,6 @@
 package com.healthfirst.classesservice.models;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.healthfirst.classesservice.enums.ClassType;
 import com.healthfirst.classesservice.validation.ClassTypeSubset;
 import java.time.LocalDateTime;
@@ -10,6 +11,7 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.Id;
+import org.springframework.data.annotation.Transient;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 @Data
@@ -18,8 +20,10 @@ import org.springframework.data.mongodb.core.mapping.Document;
 @AllArgsConstructor
 public class ClassInfo {
 
+    @Transient
+    public static final String SEQUENCE_NAME = "users_sequence";
+
     @Id
-    @NotNull(message = "id must be present")
     private Long id;
     @NotBlank(message = "className cannot be empty")
     private String className;
@@ -30,5 +34,12 @@ public class ClassInfo {
     private ClassType classType;
     @Future(message = "classTime must take place in the future")
     private LocalDateTime classTime;
+
+    public ClassInfo(String className, String instructor, ClassType classType, LocalDateTime classTime) {
+        this.className = className;
+        this.instructor = instructor;
+        this.classType = classType;
+        this.classTime = classTime;
+    }
 }
 
