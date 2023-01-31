a14d5559463da2b9d5c234c416018e013ec41a64
WillBurdett <williamburdett1995@gmail.com>
2023-01-31 12:24:14 +0000

    generic exception handling copied over

---
+++ email-service/src/main/java/com/healthfirst/emailservice/exceptions/ExceptionDetails.java
@@ -0,0 +1,15 @@
+package com.healthfirst.emailservice.exceptions;
+
+import java.time.LocalDateTime;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class ExceptionDetails {
+    private LocalDateTime timestamp;
+    private String message;
+    private String details;
+}
