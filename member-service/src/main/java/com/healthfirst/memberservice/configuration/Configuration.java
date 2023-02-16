e06ad0b2a2fc5f23b11243e1e31d7c5511c7472d
WillBurdett <williamburdett1995@gmail.com>
2023-02-16 14:18:07 +0000

    import prop re-added

--- member-service/src/main/java/com/healthfirst/memberservice/configuration/Configuration.java
+++ member-service/src/main/java/com/healthfirst/memberservice/configuration/Configuration.java
@@ -1,12 +1,16 @@
 package com.healthfirst.memberservice.configuration;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
 
+@ConfigurationProperties("member-service")
+@NoArgsConstructor
+@AllArgsConstructor
 @Data
 @Component
-@ConfigurationProperties("member-service")
 public class Configuration {
 
   private String companyEmail;
