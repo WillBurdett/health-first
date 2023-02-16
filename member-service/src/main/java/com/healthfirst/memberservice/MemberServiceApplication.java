5a63c88a89aa5cb74b1f461fb8822331424337d1
WillBurdett <williamburdett1995@gmail.com>
2023-02-16 13:07:16 +0000

    moved props again

--- member-service/src/main/java/com/healthfirst/memberservice/MemberServiceApplication.java
+++ member-service/src/main/java/com/healthfirst/memberservice/MemberServiceApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
-@RefreshScope
 @EnableFeignClients
 @SpringBootApplication
 public class MemberServiceApplication {
