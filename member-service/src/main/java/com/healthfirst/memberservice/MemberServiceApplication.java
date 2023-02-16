48f3a8830edf69977207033f03876a18f1d2c440
WillBurdett <williamburdett1995@gmail.com>
2023-02-16 12:47:37 +0000

    refresh scope annotation added

--- member-service/src/main/java/com/healthfirst/memberservice/MemberServiceApplication.java
+++ member-service/src/main/java/com/healthfirst/memberservice/MemberServiceApplication.java
@@ -2,8 +2,10 @@ package com.healthfirst.memberservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
+@RefreshScope
 @EnableFeignClients
 @SpringBootApplication
 public class MemberServiceApplication {
