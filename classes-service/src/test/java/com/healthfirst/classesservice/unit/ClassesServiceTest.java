1980a998cbd22f6ad7c8a5bcfa20c2463d728597
WillBurdett <williamburdett1995@gmail.com>
2023-03-01 11:59:57 +0000

    commented out contextLoads for dev

--- classes-service/src/test/java/com/healthfirst/classesservice/unit/ClassesServiceTest.java
+++ classes-service/src/test/java/com/healthfirst/classesservice/unit/ClassesServiceTest.java
@@ -15,6 +15,7 @@ import java.util.List;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.data.mongo.DataMongoTest;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.junit4.SpringRunner;
