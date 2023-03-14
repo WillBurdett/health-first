5829aa528ef401167f7f7b67c414d86aea381305
WillBurdett <williamburdett1995@gmail.com>
2023-03-14 10:38:53 +0000

    api gateway added and registering with naming server locally

---
+++ api-gateway/src/test/java/com/healthfirst/apigateway/ApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.healthfirst.apigateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
